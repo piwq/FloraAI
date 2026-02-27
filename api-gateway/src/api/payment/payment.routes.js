9f163efb129ddfa4e4836a682ee4de29850ef111
piwq <samushkinevg@gmail.com>
2026-02-27 20:50:38 +0500

    Init

--- api-gateway/src/api/payment/payment.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { protect } from '../../middlewares/auth.middleware.js';
-import { mockSubscribe } from './payment.controller.js';
-
-const router = Router();
-
-router.post('/mock-subscribe', protect, mockSubscribe);
-
-export default router;
--- api-gateway/src/config/statusStore.js
+++
@@ -1,9 +0,0 @@
-const statusStore = {
-  bot: {
-    status: 'DOWN',
-    lastHeartbeat: null,
-    message: 'Bot has not connected yet',
-  },
-};
-
-export default statusStore;
